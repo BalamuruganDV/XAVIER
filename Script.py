e33a70bba254c973df425b202583dfc7e7af608d
BalamuruganDV <102686521+BalamuruganDV@users.noreply.github.com>
2022-07-13 22:42:05 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -20,7 +20,7 @@ class script(object):
 - Filter is the feature were users can set automated replies for a particular keyword and luna will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. Luna should have admin privillage.
+1. Deepika-padukone-bot should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -31,11 +31,11 @@ class script(object):
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- luna Supports both url and alert inline buttons.
+- Deepika-padukone-bot Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Luna supports buttons with any telegram media type.
+2. Deepika-padukone-bot supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
@@ -66,7 +66,7 @@ class script(object):
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
-these are the extra features of Eva Maria
+these are the extra features of Deepika-padukone-bot
 
 <b>Commands and Usage:</b>
 • /id - <code>get id of a specified user.</code>
