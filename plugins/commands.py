9098d052611be2657b21c316d31f8fdebcc59864
BalamuruganDV <102686521+BalamuruganDV@users.noreply.github.com>
2022-07-16 13:32:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -45,7 +45,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
             InlineKeyboardButton('🔍 ꜱᴇᴀʀᴄʜ', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 sᴜᴘᴘᴏʀᴛ', url='https://t.me/LunaSupports')
+            InlineKeyboardButton('🤖 sᴜᴘᴘᴏʀᴛ', url='https://t.me/Rb_Supports')
         ], [
             InlineKeyboardButton('ℹ️ ʜᴇʟᴘ', callback_data='help'),
             InlineKeyboardButton('☺️ ᴀʙᴏᴜᴛ', callback_data='about')
@@ -91,7 +91,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
             InlineKeyboardButton('🔍 ꜱᴇᴀʀᴄʜ', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 sᴜᴘᴘᴏʀᴛ', url='https://t.me/LunaSupports')
+            InlineKeyboardButton('🤖 sᴜᴘᴘᴏʀᴛ', url='https://t.me/Rb_Supports')
         ], [
             InlineKeyboardButton('ℹ️ ʜᴇʟᴘ', callback_data='help'),
             InlineKeyboardButton('☺️ ᴀʙᴏᴜᴛ', callback_data='about')
