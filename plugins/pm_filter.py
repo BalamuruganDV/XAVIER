fd5bfced4af6f6728aca5745a447e817ff223df5
BalamuruganDV <102686521+BalamuruganDV@users.noreply.github.com>
2022-07-16 13:33:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -406,7 +406,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
             InlineKeyboardButton('🔍 ꜱᴇᴀʀᴄʜ', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 sᴜᴘᴘᴏʀᴛ', url='https://t.me/LunaSupports')
+            InlineKeyboardButton('🤖 sᴜᴘᴘᴏʀᴛ', url='https://t.me/Rb_Supports')
         ], [
             InlineKeyboardButton('ℹ️ ʜᴇʟᴘ', callback_data='help'),
             InlineKeyboardButton('☺️ ᴀʙᴏᴜᴛ', callback_data='about')
