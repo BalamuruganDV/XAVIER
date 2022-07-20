2656ead0e483708bdeb0f2c82650214b95d8bc1f
BalamuruganDV <102686521+BalamuruganDV@users.noreply.github.com>
2022-07-20 21:57:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/BalamuruganDV/DEEPIKA-PADUKONE.git /DEEPIKA-PADUKONE
+  git clone https://github.com/BalamuruganDV/ALONE.git /ALONE
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DEEPIKA-PADUKONE
+  git clone $UPSTREAM_REPO /ALONE
 fi
-cd /DEEPIKA-PADUKONE
+cd /ALONE
 pip3 install -U -r requirements.txt
-echo "Starting Deepika...."
+echo "Starting Alone🔥...."
 python3 bot.py
