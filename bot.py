3edca115d99c46880ed1ea4bdf2f509f741667c1
BalamuruganDV <102686521+BalamuruganDV@users.noreply.github.com>
2022-12-27 11:49:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 from aiohttp import web
-from plugins.web_server import web_server
+from plugins import web_server
 
 PORT = "8080"
 
